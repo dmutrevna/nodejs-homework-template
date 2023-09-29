a81303e11b6a03771ea4cdb904124265943abcd7
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-09-29 16:49:36 +0200

    fix hw02 edit put/delete/joi

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,11 +1,13 @@
 const Joi = require('joi')
 
 const addSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
-  email: Joi.string().email({
+  name: Joi.string().required().min(3).max(30),
+  email: Joi.string().required().email({
     minDomainSegments: 2,
   }),
-  phone: Joi.string().pattern(/^[0-9 ()-]+$/),
+  phone: Joi.string()
+    .required()
+    .pattern(/^[0-9 ()-]+$/),
 })
 
 module.exports = {
