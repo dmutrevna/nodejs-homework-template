fcb50b9e4d833006ab6192e5a57d4736fdecbfce
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-10-01 12:27:57 +0200

    fix required

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,20 +1,23 @@
 const Joi = require('joi')
 
 const addSchema = Joi.object({
-  name: Joi.string().required().min(3).max(30),
+  name: Joi.string().required().min(3).max(30).messages({
+    'any.required': 'missing required name field',
+  }),
   email: Joi.string()
     .required()
     .email({
       minDomainSegments: 2,
     })
     .messages({
-      'string.email': 'message: missing required name field',
+      'any.required': 'missing required e-mail field',
     }),
   phone: Joi.string()
     .required()
     .pattern(/^[0-9 ()-]+$/)
     .messages({
       'string.pattern.base': 'Phone number must be in format 380000000000',
+      'any.required': 'missing required phone field',
     }),
 })
 
